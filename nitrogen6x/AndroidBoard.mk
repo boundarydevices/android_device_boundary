ede30564ce069bd99c04ec611563cb5fb268cb1c
Eric Nelson <eric.nelson@boundarydevices.com>
2013-01-05 19:41:08 -0700

    add RAM disk targets
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- nitrogen6x/AndroidBoard.mk
+++ nitrogen6x/AndroidBoard.mk
@@ -5,10 +5,5 @@ include device/fsl-proprietary/codec/fsl-codec.mk
 endif
 
 include device/boundary/bootscript.mk
-
-#
-# The ramdisk.mk targets are broken.
-# See comments in http://boundarydevices.com/android-r13-4-stage-3/ for details
-#
-#include device/boundary/ramdisk.mk
+include device/boundary/ramdisk.mk
 
