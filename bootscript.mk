0d40600c0d49b7172994457f0e1ca5ff23d04317
Eric Nelson <eric.nelson@boundarydevices.com>
2013-07-19 18:21:26 -0700

    always get 6x_upgrade.txt from nitrogen6x directory

--- bootscript.mk
+++ bootscript.mk
@@ -4,7 +4,7 @@ $(BOOTSCRIPT_TARGET): bootable/bootloader/uboot-imx/board/boundary/$(TARGET_BOOT
 	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
 
 UPGRADE_TARGET := $(PRODUCT_OUT)/boot/6x_upgrade
-$(UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/$(TARGET_BOOTLOADER_DIR)/6x_upgrade.txt
+$(UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/nitrogen6x/6x_upgrade.txt
 	mkdir -p $(dir $@)
 	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot loader upgrade script" -d $< $@
 
