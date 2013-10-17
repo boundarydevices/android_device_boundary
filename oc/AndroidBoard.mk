9d9d3fc0254565eae8e2ac5b543b763eb59a8919
Eric Nelson <eric.nelson@boundarydevices.com>
2013-10-17 09:04:06 -0700

    oc: use nitrogen6x boot script
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- oc/AndroidBoard.mk
+++ oc/AndroidBoard.mk
@@ -4,7 +4,7 @@ ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
 include device/fsl-proprietary/codec/fsl-codec.mk
 endif
 
-TARGET_BOOTLOADER_DIR=oc
+TARGET_BOOTLOADER_DIR=nitrogen6x
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
 
