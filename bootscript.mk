de8656c12dd5612a8e97b666def5bd6e623bd465
Eric Nelson <eric.nelson@boundarydevices.com>
2013-11-18 09:56:03 -0700

    bootscript.mk: remove dependency on build system's mkimage

--- bootscript.mk
+++ bootscript.mk
@@ -1,15 +1,15 @@
 BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot/6x_bootscript
-$(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_BOOTLOADER_DIR)/6x_bootscript_jb.txt
+$(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_BOOTLOADER_DIR)/6x_bootscript_jb.txt $(TARGET_BOOTLOADER_IMAGE)
 	mkdir -p $(dir $@)
-	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
+	$(TOPDIR)bootable/bootloader/uboot-imx/tools/mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
 
 UPGRADE_TARGET := $(PRODUCT_OUT)/boot/6x_upgrade
 $(UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/nitrogen6x/6x_upgrade.txt
 	mkdir -p $(dir $@)
-	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot loader upgrade script" -d $< $@
+	$(TOPDIR)bootable/bootloader/uboot-imx/tools/mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot loader upgrade script" -d $< $@
 
 .PHONY: bootscript
-bootscript: $(BOOTSCRIPT_TARGET) $(UPGRADE_TARGET)
+bootscript: $(BOOTSCRIPT_TARGET) $(UPGRADE_TARGET) $(TARGET_BOOTLOADER_IMAGE)
 
 droidcore: bootscript
 
