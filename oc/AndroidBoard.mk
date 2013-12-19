d1d74e18a4a4a923c5e1ba51cb442b560af49d5a
Eric Nelson <eric.nelson@boundarydevices.com>
2013-12-19 13:02:12 -0700

    oc: use oc-specific boot script
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- oc/AndroidBoard.mk
+++ oc/AndroidBoard.mk
@@ -4,7 +4,7 @@ ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
 include device/fsl-proprietary/codec/fsl-codec.mk
 endif
 
-TARGET_BOOTLOADER_DIR=nitrogen6x
+TARGET_BOOTLOADER_DIR=oc
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
 
