edfbf7661e28f928f439d2ee2698324a7327a015
Eric Nelson <eric.nelson@boundarydevices.com>
2014-01-11 10:30:15 -0700

    bootscript: make bootscript conditional on TARGET_BOOTLOADER_IMAGE
    Commit 92ff85e8 made creation of the boot script conditional on
    u-boot.imx. It did this in order to ensure that the tool mkimage
    has been built, removing the need to install u-boot-tools on
    the build machine.
    
    The trouble is that the latest build structure can build multiple
    U-Boot images for Quad Core, Solo, and various memory configurations.
    
    Using TARGET_BOOTLOADER_IMAGE should achieve the same thing without
    a reference to u-boot.imx (which is ambiguous at best).
    
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- bootscript.mk
+++ bootscript.mk
@@ -1,10 +1,10 @@
 BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot/6x_bootscript
-$(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_BOOTLOADER_DIR)/6x_bootscript_jb.txt $(PRODUCT_OUT)/u-boot.imx
+$(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_BOOTLOADER_DIR)/6x_bootscript_jb.txt $(TARGET_BOOTLOADER_IMAGE)
 	mkdir -p $(dir $@)
 	$(TOPDIR)bootable/bootloader/uboot-imx/tools/mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
 
 UPGRADE_TARGET := $(PRODUCT_OUT)/boot/6x_upgrade
-$(UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/nitrogen6x/6x_upgrade.txt $(PRODUCT_OUT)/u-boot.imx
+$(UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/nitrogen6x/6x_upgrade.txt $(TARGET_BOOTLOADER_IMAGE)
 	mkdir -p $(dir $@)
 	$(TOPDIR)bootable/bootloader/uboot-imx/tools/mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot loader upgrade script" -d $< $@
 
