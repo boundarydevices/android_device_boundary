d742a18dbe1a908674648b72a0133965c2cf255d
Eric Nelson <eric.nelson@boundarydevices.com>
2014-04-02 15:39:11 -0700

    nitrogen6x: Add script, startup to expose GPIO pins 125,126
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- nitrogen6x/nitrogen6x.mk
+++ nitrogen6x/nitrogen6x.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
 	device/boundary/wl12xx/TIInit_7.2.31.bts:system/etc/firmware/TIInit_7.2.31.bts \
 	device/boundary/nitrogen6x/audio_policy.conf:system/etc/audio_policy.conf \
 	device/boundary/nitrogen6x/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+        device/boundary/expose-leds:system/bin/expose-leds \
 
 PRODUCT_PROPERTY_OVERRIDES += \
        wifi.interface=wlan0 \
