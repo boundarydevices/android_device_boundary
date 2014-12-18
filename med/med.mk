6f4a5000cbff5073bebbe2aa9951c7e99b3c6489
Eric Nelson <eric.nelson@boundarydevices.com>
2014-12-18 10:35:34 -0700

    med: fix Superuser package, add memtool
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- med/med.mk
+++ med/med.mk
@@ -11,7 +11,6 @@ PRODUCT_COPY_FILES += \
 	device/boundary/med/required_hardware.xml:system/etc/permissions/required_hardware.xml \
 	device/boundary/med/init.rc:root/init.freescale.rc \
 	device/boundary/med/init.rc:root/init.boundary.rc \
-        device/boundary/init.superuser.rc:root/init.superuser.rc \
 	device/boundary/med/ueventd.boundary.rc:root/ueventd.freescale.rc \
 	device/boundary/med/vold.fstab:system/etc/vold.fstab \
 	device/boundary/med/fstab.boundary:root/fstab.boundary \
@@ -36,4 +35,8 @@ PRODUCT_AAPT_CONFIG += xlarge large tvdpi hdpi
 
 include device/boundary/openssh.mk
 
-PRODUCT_PACKAGES += ethernet
+SUPERUSER_PACKAGE := com.boundary.superuser
+SUPERUSER_EMBEDDED := true
+
+PRODUCT_PACKAGES += ethernet \
+	memtool
