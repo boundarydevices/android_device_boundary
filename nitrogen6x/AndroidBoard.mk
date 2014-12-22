15a44a174ade10fb42baf31a78646d5d3fb43ff6
Eric Nelson <eric.nelson@boundarydevices.com>
2014-12-22 14:19:57 -0700

    nitrogen6x: add dtb files
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- nitrogen6x/AndroidBoard.mk
+++ nitrogen6x/AndroidBoard.mk
@@ -6,6 +6,7 @@ endif
 include device/fsl-proprietary/media-profile/media-profile.mk
 
 TARGET_BOOTLOADER_DIR=nitrogen6x
+TARGET_BOARD_DTS_CONFIG=imx6q:imx6q-nitrogen6x.dtb imx6dl:imx6dl-nitrogen6x.dtb imx6q:imx6q-nitrogen6_max.dtb imx6q:imx6q-sabrelite.dtb imx6dl:imx6dl-nitrogen6_vm.dtb
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
 
