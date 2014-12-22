333d2e445ee9c892a29451cec903d0470c160c64
Eric Nelson <eric.nelson@boundarydevices.com>
2014-12-22 14:19:57 -0700

    nit6xlite: add dtb files
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- nit6xlite/AndroidBoard.mk
+++ nit6xlite/AndroidBoard.mk
@@ -6,6 +6,7 @@ endif
 include device/fsl-proprietary/media-profile/media-profile.mk
 
 TARGET_BOOTLOADER_DIR=nit6xlite
+TARGET_BOARD_DTS_CONFIG=imx6dl:imx6dl-nit6xlite.dtb
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
 
