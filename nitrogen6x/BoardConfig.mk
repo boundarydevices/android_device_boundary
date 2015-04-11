23ffbae71e4573e422d4646b092c025b4c9681d0
Eric Nelson <eric.nelson@boundarydevices.com>
2015-04-11 10:16:45 -0700

    nitrogen6x: use wl12xx.ko instead of wlcore_sdio.ko for WL127x
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- nitrogen6x/BoardConfig.mk
+++ nitrogen6x/BoardConfig.mk
@@ -16,7 +16,7 @@ endif
 TARGET_RECOVERY_FSTAB := device/boundary/nitrogen6x/fstab.freescale
 
 TARGET_KERNEL_MODULES := \
-    kernel_imx/drivers/net/wireless/ti/wlcore/wlcore_sdio.ko:system/lib/modules/wlcore_sdio.ko \
+    kernel_imx/drivers/net/wireless/ti/wl12xx/wl12xx.ko:system/lib/modules/wl12xx.ko \
 
 BOARD_HAS_SGTL5000 := true
 BOARD_HAVE_BLUETOOTH := true
@@ -55,8 +55,8 @@ BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
-WIFI_DRIVER_MODULE_NAME          := "wlcore_sdio"
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlcore_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx.ko"
 
 # WiFi Direct requirements
 WPA_BUILD_HOSTAPD         := true
