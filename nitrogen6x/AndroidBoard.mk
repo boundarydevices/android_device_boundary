bff64837796c95ff8ea9e582e8c927de73e1f3bf
Gary Bisson <gary.bisson@boundarydevices.com>
2016-02-12 17:45:54 +0100

    nitrogen6x: add support for Nitrogen6 SOMv2
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen6x/AndroidBoard.mk
+++ nitrogen6x/AndroidBoard.mk
@@ -6,7 +6,11 @@ endif
 include device/fsl-proprietary/media-profile/media-profile.mk
 
 TARGET_BOOTLOADER_DIR=nitrogen6x
-TARGET_BOARD_DTS_CONFIG=imx6q:imx6q-nitrogen6x.dtb imx6dl:imx6dl-nitrogen6x.dtb imx6q:imx6q-nitrogen6_max.dtb imx6qp:imx6qp-nitrogen6_max.dtb imx6q:imx6q-sabrelite.dtb imx6dl:imx6dl-nitrogen6_vm.dtb
+TARGET_BOARD_DTS_CONFIG= \
+	imx6q:imx6q-nitrogen6x.dtb imx6dl:imx6dl-nitrogen6x.dtb imx6q:imx6q-nitrogen6_max.dtb \
+	imx6qp:imx6qp-nitrogen6_max.dtb imx6q:imx6q-sabrelite.dtb imx6dl:imx6dl-nitrogen6_vm.dtb \
+	imx6q:imx6q-nitrogen6_som2.dtb
+
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
 
