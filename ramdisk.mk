906dd5b76b6b681996cca4f138bfe833cfa7b033
Gary Bisson <gary.bisson@boundarydevices.com>
2016-02-25 19:00:09 +0100

    ramdisk.mk: remove recovery ramdisk creation
    It now needs to be done during the recovery.img creation and not after.
    This change allows to have an ext4 version of recovery.img in order to
    flash it using fastboot.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- ramdisk.mk
+++ ramdisk.mk
@@ -3,11 +3,4 @@ $(RAMDISK_TARGET): $(PRODUCT_OUT)/ramdisk.img kernelmodules
 	mkdir -p $(dir $@)
 	mkimage -A arm -O linux -T ramdisk -n "RAM Disk" -d $< $@
 
-RAMDISK_RECOVERY_TARGET := $(PRODUCT_OUT)/uramdisk-recovery.img
-$(RAMDISK_RECOVERY_TARGET): $(PRODUCT_OUT)/recovery.img
-	mkdir -p $(dir $@)
-	mkimage -A arm -O linux -T ramdisk -n "RAM Disk" -d $(PRODUCT_OUT)/ramdisk-recovery.img $@
-
-droidcore: $(RAMDISK_TARGET) $(RAMDISK_RECOVERY_TARGET)
-bootimage: $(RAMDISK_TARGET) $(RAMDISK_RECOVERY_TARGET)
-
+bootimage: $(RAMDISK_TARGET)
