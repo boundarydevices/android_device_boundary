be3ba18dd8e66f36a6a945ed8ac9fc94a1d69a8d
Gary Bisson <gary.bisson@boundarydevices.com>
2016-05-24 18:01:29 +0200

    sepolicy.mk: remove unnecessary union
    As said during the build when union exists:
    BOARD_SEPOLICY_UNION is no longer required - all files found in
    BOARD_SEPOLICY_DIRS are implicitly unioned.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,29 +1,3 @@
 BOARD_SEPOLICY_DIRS := \
        device/boundary/sepolicy \
        device/fsl/imx6/sepolicy
-
-BOARD_SEPOLICY_UNION := \
-       domain.te \
-       system_app.te \
-       system_server.te \
-       untrusted_app.te \
-       sensors.te \
-       init_shell.te \
-       bluetooth.te \
-       hci_attach.te \
-       kernel.te \
-       mediaserver.te \
-       file_contexts \
-       genfs_contexts \
-       fs_use  \
-       rild.te \
-       hostapd.te \
-       init.te \
-       netd.te \
-       bootanim.te \
-       dnsmasq.te \
-       recovery.te \
-       device.te \
-       wpa.te \
-       zygote.te
-
