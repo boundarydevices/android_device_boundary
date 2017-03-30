1903ae2f8140883965a76b1096256ab3d3c1e33e
Gary Bisson <gary.bisson@boundarydevices.com>
2017-03-30 18:45:20 +0200

    nitrogen6x: AndroidBoard.mk: add support for QuadPlus version of SOM2
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen6x/AndroidBoard.mk
+++ nitrogen6x/AndroidBoard.mk
@@ -9,7 +9,7 @@ TARGET_BOOTLOADER_DIR=nitrogen6x
 TARGET_BOARD_DTS_CONFIG= \
 	imx6q:imx6q-nitrogen6x.dtb imx6dl:imx6dl-nitrogen6x.dtb imx6q:imx6q-nitrogen6_max.dtb \
 	imx6qp:imx6qp-nitrogen6_max.dtb imx6q:imx6q-sabrelite.dtb imx6dl:imx6dl-nitrogen6_vm.dtb \
-	imx6q:imx6q-nitrogen6_som2.dtb
+	imx6qp:imx6qp-nitrogen6_som2.dtb imx6q:imx6q-nitrogen6_som2.dtb
 
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
