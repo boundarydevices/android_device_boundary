daeef3ce4d1a5fa7c7975d45636ace3d021c8422
Gary Bisson <gary.bisson@boundarydevices.com>
2017-03-31 11:34:14 +0200

    dnx: update to n7.1.1_1.0.0-ga release
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- dnx/BoardConfig.mk
+++ dnx/BoardConfig.mk
@@ -10,10 +10,22 @@ include device/fsl/imx6/BoardConfigCommon.mk
 ifneq ($(DEFCONF),)
 TARGET_KERNEL_DEFCONF := $(DEFCONF)
 else
-TARGET_KERNEL_DEFCONF := boundary_defconfig
+TARGET_KERNEL_DEFCONF := boundary_android_defconfig
 endif
 
 TARGET_RECOVERY_FSTAB := device/boundary/dnx/fstab.freescale
+
+TARGET_OTA_BLOCK_DISABLED := true
+
+TARGET_COPY_OUT_VENDOR := vendor
+
+# override Freescale partition sizes to match our flashing script
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2097152000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 536870912
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 10485760
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # boot.img & recovery.img creation
 TARGET_BOOTIMAGE_USE_EXT4 := true
@@ -29,15 +41,13 @@ BOARD_NOT_HAVE_MODEM := true
 BOARD_HAVE_IMX_CAMERA := true
 BOARD_HAVE_USB_CAMERA := false
 BOARD_HAS_SENSOR := false
-TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
 USE_ION_ALLOCATOR := false
 USE_GPU_ALLOCATOR := true
 
 # camera hal v3
 IMX_CAMERA_HAL_V3 := true
-
-include device/fsl-proprietary/gpu-viv/fsl-gpu.mk
 
 BUILD_TARGET_FS ?= ext4
 include device/fsl/imx6/imx6_target_fs.mk
@@ -65,7 +75,6 @@ WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx.ko"
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
 USES_TI_MAC80211                 := true
-COMMON_GLOBAL_CFLAGS             += -DUSES_TI_MAC80211
 BOARD_HAVE_BLUETOOTH_TI          := true
 TARGET_KERNEL_MODULES := \
     kernel_imx/drivers/net/wireless/ti/wl12xx/wl12xx.ko:system/lib/modules/wl12xx.ko
@@ -83,3 +92,6 @@ BOARD_SU_ALLOW_ALL := true
 
 include device/boundary/sepolicy.mk
 
+BOARD_SECCOMP_POLICY += device/boundary/dnx/seccomp
+
+TARGET_BOARD_KERNEL_HEADERS := device/fsl/common/kernel-headers
--- dnx/dnx.mk
+++ dnx/dnx.mk
@@ -7,9 +7,11 @@ PRODUCT_DEVICE := dnx
 PRODUCT_BRAND := boundary
 PRODUCT_MANUFACTURER := boundary
 
+USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
 	device/boundary/common/audio_policy.conf:system/etc/audio_policy.conf \
-	device/boundary/common/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+	device/boundary/common/audio_effects.conf:vendor/etc/audio_effects.conf \
+	device/boundary/common/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
 	device/boundary/common/init.rc:root/init.freescale.rc \
 	device/boundary/common/init.recovery.rc:root/init.recovery.freescale.rc \
 	device/boundary/dnx/bootinit.sh:system/bin/bootinit \
@@ -21,6 +23,11 @@ PRODUCT_COPY_FILES += \
 	device/boundary/dnx/ueventd.freescale.rc:root/ueventd.freescale.rc \
 	device/boundary/dnx/media/bootanimation.zip:system/media/bootanimation.zip \
 	device/boundary/dnx/media/bootup.wav:system/media/bootup.wav \
+	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
 	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin 	\
 	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
 
@@ -68,3 +75,19 @@ PRODUCT_PACKAGES += \
 	ethernet \
 	su \
 	CMFileManager
+
+PRODUCT_COPY_FILES += \
+    device/fsl-proprietary/gpu-viv/lib/egl/egl.cfg:system/lib/egl/egl.cfg
+
+PRODUCT_PACKAGES += \
+    libEGL_VIVANTE \
+    libGLESv1_CM_VIVANTE \
+    libGLESv2_VIVANTE \
+    gralloc_viv.imx6 \
+    hwcomposer_viv.imx6 \
+    hwcomposer_fsl.imx6 \
+    libGAL \
+    libGLSLC \
+    libVSC \
+    libg2d \
+    libgpuhelper
