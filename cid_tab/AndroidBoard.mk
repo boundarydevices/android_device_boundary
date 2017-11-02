983c40a24d4dd36268ad57523ee040ee67f81ebf
Gary Bisson <gary.bisson@boundarydevices.com>
2017-11-02 11:29:27 +0100

    cid_tab: build bootloader automatically
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- cid_tab/AndroidBoard.mk
+++ cid_tab/AndroidBoard.mk
@@ -6,6 +6,8 @@ endif
 include device/fsl-proprietary/media-profile/media-profile.mk
 
 TARGET_BOOTLOADER_DIR=cid_tab
+TARGET_BOOTLOADER_POSTFIX := imx
+TARGET_BOOTLOADER_CONFIG := cid_tab_q2g_defconfig
 TARGET_BOARD_DTS_CONFIG= \
 	imx6q:imx6q-cid_tab.dtb
 
