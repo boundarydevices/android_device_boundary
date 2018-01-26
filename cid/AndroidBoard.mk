c11cfb46a9ff01761712625b4128b767cfc9239e
Gary Bisson <gary.bisson@boundarydevices.com>
2018-01-26 09:53:22 +0100

    cid*: disable U-Boot build for now
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- cid/AndroidBoard.mk
+++ cid/AndroidBoard.mk
@@ -7,7 +7,7 @@ include device/fsl-proprietary/media-profile/media-profile.mk
 
 TARGET_BOOTLOADER_DIR=cid
 TARGET_BOOTLOADER_POSTFIX := imx
-TARGET_BOOTLOADER_CONFIG := cid_2g_defconfig
+#TARGET_BOOTLOADER_CONFIG := cid_2g_defconfig
 TARGET_BOARD_DTS_CONFIG= \
 	imx6q:imx6q-cid.dtb
 
