6751c3b32874a37d633e72c64a7708020fab395b
Gary Bisson <gary.bisson@boundarydevices.com>
2018-03-20 19:03:40 +0100

    bootscript.mk: drop legacy files and rename to standard
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- bootscript.mk
+++ bootscript.mk
@@ -1,21 +1,15 @@
-BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot/6x_bootscript
-$(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_BOOTLOADER_DIR)/6x_bootscript.txt
+BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot/boot.scr
+$(BOOTSCRIPT_TARGET): device/boundary/$(TARGET_PRODUCT)/bootscript.txt
 	mkdir -p $(dir $@)
 	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
 
 UPGRADE_TARGET := $(PRODUCT_OUT)/boot/upgrade.scr
-$(UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/bootscripts/upgrade.txt
+$(UPGRADE_TARGET): $(UBOOT_IMX_PATH)/uboot-imx/board/boundary/bootscripts/upgrade.txt
 	mkdir -p $(dir $@)
 	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "upgrade script" -d $< $@
 
-OLD_UPGRADE_TARGET := $(PRODUCT_OUT)/boot/6x_upgrade
-$(OLD_UPGRADE_TARGET): bootable/bootloader/uboot-imx/board/boundary/nitrogen6x/6x_upgrade.txt
-	mkdir -p $(dir $@)
-	mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot loader upgrade script" -d $< $@
-
 .PHONY: bootscript
-bootscript: $(BOOTSCRIPT_TARGET) $(UPGRADE_TARGET) $(OLD_UPGRADE_TARGET)
+bootscript: $(BOOTSCRIPT_TARGET) $(UPGRADE_TARGET)
 
 droidcore: bootscript
 bootimage: bootscript
-
