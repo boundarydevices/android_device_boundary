6092a14f3e6b6412c62a494ed176a13eeec68e62
Gary Bisson <gary.bisson@boundarydevices.com>
2018-03-23 09:46:55 +0100

    cid: add cid2 U-Boot and dtb targets
    Those are for rev4 devices with different LED setup.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- cid/AndroidBoard.mk
+++ cid/AndroidBoard.mk
@@ -7,9 +7,9 @@ include device/fsl-proprietary/media-profile/media-profile.mk
 
 TARGET_BOOTLOADER_DIR=cid
 TARGET_BOOTLOADER_POSTFIX := imx
-#TARGET_BOOTLOADER_CONFIG := cid_2g_defconfig
+#TARGET_BOOTLOADER_CONFIG := cid_2g_defconfig cid2_q2g_defconfig
 TARGET_BOARD_DTS_CONFIG= \
-	imx6q:imx6q-cid.dtb
+	imx6q:imx6q-cid.dtb imx6q:imx6q-cid2.dtb
 
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
