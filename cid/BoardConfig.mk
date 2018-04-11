0a695345662a87a79cacb7aafd672367074bc066
Gary Bisson <gary.bisson@boundarydevices.com>
2018-04-11 15:45:51 +0200

    cid: update to o8.0.0_1.0.0_ga release
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- cid/BoardConfig.mk
+++ cid/BoardConfig.mk
@@ -4,15 +4,20 @@
 
 include device/fsl/imx6/soc/imx6dq.mk
 export BUILD_ID=1.0.0-ga
-export BUILD_NUMBER=20170628
+export BUILD_NUMBER=$(shell date +%Y%m%d)
 include device/fsl/imx6/BoardConfigCommon.mk
+ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
+-include $(FSL_CODEC_PATH)/fsl-codec/fsl-codec.mk
+endif
 
 TARGET_BOOTLOADER_BOARD_NAME := cid
-ifneq ($(DEFCONF),)
-TARGET_KERNEL_DEFCONF := $(DEFCONF)
-else
+TARGET_BOOTLOADER_DIR=cid
+TARGET_BOOTLOADER_POSTFIX := imx
+#TARGET_BOOTLOADER_CONFIG := cid_2g_defconfig cid2_q2g_defconfig
+
 TARGET_KERNEL_DEFCONF := boundary_android_defconfig
-endif
+TARGET_BOARD_DTS_CONFIG= \
+	imx6q:imx6q-cid.dtb imx6q:imx6q-cid2.dtb
 
 TARGET_RECOVERY_FSTAB := device/boundary/cid/fstab.freescale
 
@@ -21,11 +26,11 @@ TARGET_OTA_BLOCK_DISABLED := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # override Freescale partition sizes to match our flashing script
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5872008704
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1778368000
 BOARD_CACHEIMAGE_PARTITION_SIZE := 536870912
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_PARTITION_SIZE := 10485760
+BOARD_VENDORIMAGE_PARTITION_SIZE := 67108864
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # boot.img & recovery.img creation
@@ -36,28 +41,33 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20940800
 
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
-# camera hal v3
+BUILD_TARGET_FS ?= ext4
+include device/fsl/imx6/imx6_target_fs.mk
+
+BOARD_NOT_HAVE_MODEM := true
+BOARD_HAS_SENSOR := false
+IMX6_CONSUMER_IR_HAL := false
+
+# Camera
+BOARD_HAVE_IMX_CAMERA := true
+BOARD_HAVE_USB_CAMERA := false
 IMX_CAMERA_HAL_V3 := true
 USE_CAMERA_STUB := false
 BOARD_HAVE_FLASHLIGHT := true
-
-BOARD_NOT_HAVE_MODEM := true
-BOARD_HAVE_IMX_CAMERA := true
-BOARD_HAVE_USB_CAMERA := false
 BOARD_HAS_BATTERY_LIGHTS := true
-BOARD_HAS_SENSOR := false
-
-USE_ION_ALLOCATOR := false
-USE_GPU_ALLOCATOR := true
-
-BUILD_TARGET_FS ?= ext4
-include device/fsl/imx6/imx6_target_fs.mk
 
 PRODUCT_MODEL := CID
 
 # for recovery service
 TARGET_SELECT_KEY := 28
-TARGET_USERIMAGES_USE_EXT4 := true
+
+# Define frame buffer count to match IPU
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# GPU configuration
+USE_ION_ALLOCATOR := true
+USE_GPU_ALLOCATOR := false
+BOARD_EGL_CFG := $(FSL_PROPRIETARY_PATH)/fsl-proprietary/gpu-viv/lib/egl/egl.cfg
 
 # WiFi/BT common defines
 BOARD_HAVE_WIFI                  := true
@@ -69,9 +79,6 @@ BOARD_HOSTAPD_DRIVER             := NL80211
 
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 BOARD_WLAN_DEVICE                := qcwcn
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/qcacld_wlan.ko"
-WIFI_DRIVER_MODULE_NAME          := "wlan"
-WIFI_DRIVER_MAC_PROP             := "ro.boot.wlan.mac"
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
 BOARD_HAVE_BLUETOOTH_QCOM        := true
 BOARD_SUPPORTS_BLE_VND           := true
@@ -83,7 +90,5 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/boundary/cid/
 
 include device/boundary/sepolicy.mk
 include hardware/sierra/sepolicy.mk
-
-BOARD_SECCOMP_POLICY += device/boundary/cid/seccomp
 
 TARGET_BOARD_KERNEL_HEADERS := device/fsl/common/kernel-headers
