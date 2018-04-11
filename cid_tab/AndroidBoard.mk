cc3cc952d6e07c48071abfaca3d3d3d261a2ea3f
Gary Bisson <gary.bisson@boundarydevices.com>
2018-04-11 15:55:07 +0200

    cid_tab: update to o8.0.0_1.0.0_ga release
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- cid_tab/AndroidBoard.mk
+++ cid_tab/AndroidBoard.mk
@@ -1,16 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
--include device/fsl-codec/fsl-codec.mk
-endif
-include device/fsl-proprietary/media-profile/media-profile.mk
-
-TARGET_BOOTLOADER_DIR=cid_tab
-TARGET_BOOTLOADER_POSTFIX := imx
-#TARGET_BOOTLOADER_CONFIG := cid_tab_q2g_defconfig
-TARGET_BOARD_DTS_CONFIG= \
-	imx6q:imx6q-cid_tab.dtb
-
 include device/boundary/bootscript.mk
 include device/boundary/ramdisk.mk
-
+include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media-profile.mk
--- cid_tab/cid_tab.mk
+++ cid_tab/cid_tab.mk
@@ -1,3 +1,4 @@
+-include device/fsl/common/imx_path/ImxPathConfig.mk
 $(call inherit-product, device/fsl/imx6/imx6.mk)
 $(call inherit-product-if-exists,hardware/sierra/user_tags.mk)
 $(call inherit-product-if-exists,vendor/google/products/gms.mk)
@@ -7,28 +8,97 @@ PRODUCT_NAME := cid_tab
 PRODUCT_DEVICE := cid_tab
 PRODUCT_BRAND := boundary
 PRODUCT_MANUFACTURER := boundary
+KERNEL_IMX_PATH := vendor/boundary
+UBOOT_IMX_PATH := vendor/boundary
 
+# Audio
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
+	device/boundary/common/audio_effects.conf:vendor/etc/audio_effects.conf \
+	device/boundary/common/audio_policy_configuration.xml:vendor/etc/audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:vendor/etc/a2dp_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:vendor/etc/r_submix_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:vendor/etc/usb_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:vendor/etc/default_volume_tables.xml \
+	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:vendor/etc/audio_policy_volumes.xml
+
+# Init and recovery general configuration
+PRODUCT_COPY_FILES += \
+	device/boundary/common/apns-full-conf.xml:vendor/etc/apns-conf.xml \
 	device/boundary/common/init.rc:root/init.freescale.rc \
 	device/boundary/common/init.recovery.rc:root/init.recovery.freescale.rc \
 	device/boundary/cid_tab/init.i.MX6Q.rc:root/init.freescale.i.MX6Q.rc \
-	device/boundary/cid_tab/required_hardware.xml:system/etc/permissions/required_hardware.xml \
+	device/boundary/cid_tab/required_hardware.xml:vendor/etc/permissions/required_hardware.xml \
 	device/boundary/cid_tab/ueventd.freescale.rc:root/ueventd.freescale.rc \
 	device/boundary/cid_tab/fstab.freescale:root/fstab.freescale \
+	device/boundary/cid/ota.conf:vendor/etc/ota.conf
+
+# Input configuration
+PRODUCT_COPY_FILES += \
 	device/fsl/common/input/eGalax_Touch_Screen.idc:system/usr/idc/Goodix_Capacitive_TouchScreen.idc \
-	device/boundary/common/apns-full-conf.xml:system/etc/apns-conf.xml \
-	device/boundary/common/audio_policy.conf:system/etc/audio_policy.conf \
-	device/boundary/common/audio_effects.conf:vendor/etc/audio_effects.conf \
-	device/boundary/common/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-	device/boundary/cid/ota.conf:system/etc/ota.conf \
-	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
-	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
-	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
-	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin 	\
-	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
+
+# VPU firmware
+PRODUCT_COPY_FILES += \
+	$(LINUX_FIRMWARE_PATH)/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:vendor/lib/firmware/vpu/vpu_fw_imx6d.bin \
+	$(LINUX_FIRMWARE_PATH)/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:vendor/lib/firmware/vpu/vpu_fw_imx6q.bin
+
+# Vendor seccomp policy files for media components:
+PRODUCT_COPY_FILES += \
+	device/boundary/cid/seccomp/mediacodec-seccomp.policy:vendor/etc/seccomp_policy/mediacodec.policy \
+	device/boundary/cid/seccomp/mediaextractor-seccomp.policy:vendor/etc/seccomp_policy/mediaextractor.policy
+
+# Vendor Interface Manifest
+PRODUCT_COPY_FILES += \
+	device/boundary/cid/manifest.xml:vendor/manifest.xml
+
+# HWC2 HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl
+
+# Gralloc HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# Audio HAL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
+
+# Light HAL
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
+# WiFi HAL
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    wifilogd \
+    wificond
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=160
@@ -57,22 +127,6 @@ PRODUCT_COPY_FILES += \
 
 BOARD_CUSTOM_BT_CONFIG := device/boundary/cid_tab/libbt_vnd_cid_tab.conf
 
-# ExFat support
-PRODUCT_PACKAGES += \
-	fsck.exfat \
-	libfuse \
-	mkfs.exfat \
-	mount.exfat
-
-PRODUCT_PACKAGES += \
-	dhcpcd.conf \
-	hostapd.conf
-
-PRODUCT_PACKAGES += \
-	CMFileManager \
-	Ethernet \
-	su
-
 PRODUCT_PACKAGES += \
 	sensors.cid_tab
 
@@ -85,18 +139,22 @@ PRODUCT_PACKAGES += \
 	fs_config_dirs \
 	fs_config_files
 
-PRODUCT_COPY_FILES += \
-	device/fsl-proprietary/gpu-viv/lib/egl/egl.cfg:system/lib/egl/egl.cfg
+# Misc packages
+PRODUCT_PACKAGES += \
+	dhcpcd.conf \
+	hostapd.conf \
+	su
 
+# GPU packages
 PRODUCT_PACKAGES += \
 	libEGL_VIVANTE \
 	libGLESv1_CM_VIVANTE \
 	libGLESv2_VIVANTE \
-	gralloc_viv.imx6 \
-	hwcomposer_viv.imx6 \
-	hwcomposer_fsl.imx6 \
 	libGAL \
 	libGLSLC \
 	libVSC \
 	libg2d \
 	libgpuhelper
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.frp.pst=/dev/block/bootdevice/by-name/frp
