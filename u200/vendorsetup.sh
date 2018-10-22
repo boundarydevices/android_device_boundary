145cde62fc42fd74625fbb9252e49fdf046a3115
Luan Yuan <luan.yuan@amlogic.com>
2018-10-22 14:38:57 +0800

    device: add support u200 u221 [1/1]
    PD#SWPL-914
    
    Problem:
    lack of u200 u221 config
    
    Solution:
    add them.
    
    Verify:
    verify by u200 u221
    
    Change-Id: I40d4df5677901b4d8aefc2437c1d6a75ec6ab5d2
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

---
+++ u200/vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo u200-eng
+add_lunch_combo u200-user
+add_lunch_combo u200-userdebug
---
+++ u221/vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo u221-eng
+add_lunch_combo u221-user
+add_lunch_combo u221-userdebug
