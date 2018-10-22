49d3885699833542ce9389b6d9b03b4206f9fb60
Wencai You <wencai.you@amlogic.com>
2018-10-22 17:16:28 +0800

    device: Shipping api 28 need update manifest [1/1]
    PD#SWPL-866
    
    Problem:
    fail VtsTrebleVendorVintfTest#
    DeviceManifestTest.ShippingFcmVersion_32bit
    
    Solution:
    update manifest
    
    Verify:
    verify by ampere
    
    Change-Id: I88d2a450b134c2601f1d4de387989c4d295fbcae
    Signed-off-by: Wencai You <wencai.you@amlogic.com>

--- common/core_amlogic.mk
+++ common/core_amlogic.mk
@@ -370,8 +370,9 @@ PRODUCT_COPY_FILES += \
 
 #Audio HAL
 PRODUCT_PACKAGES += \
-     android.hardware.audio@2.0-impl \
-     android.hardware.audio.effect@2.0-impl \
+     android.hardware.audio@4.0-impl:32 \
+     android.hardware.audio.effect@4.0-impl:32 \
+     android.hardware.soundtrigger@2.1-impl:32 \
      android.hardware.audio@2.0-service
 #Camera HAL
 #ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
