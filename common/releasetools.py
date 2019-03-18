fc82391fd358d0e5782ec358536e17cb8b3ac693
Zhigang Yu <zhigang.yu@amlogic.com>
2019-03-18 15:39:35 +0800

    ota: delete no need python code [1/1]
    PD#OTT-2737
    
    Problem:
    will cause make incremental ota package failed
    
    Solution:
    delete the unused python code
    
    Verify:
    android P on ampere
    
    Change-Id: Ifda881b52ffc0a22fa7e9ea5bcc9af49e715f025
    Signed-off-by: Zhigang Yu <zhigang.yu@amlogic.com>

--- common/releasetools.py
+++ common/releasetools.py
@@ -33,44 +33,12 @@ def SetBootloaderEnv(script, name, val):
   """Set bootloader env name with val."""
   script.AppendExtra('set_bootloader_env("%s", "%s");' % (name, val))
 
-def LoadInfoDict_amlogic(info_dict, input_file, input_dir=None):
-  """Read and parse the META/misc_info.txt key/value pairs from the
-  input target files and return a dict."""
-
-  data = input_file.read("VENDOR/build.prop")
-  data += input_file.read("VENDOR/default.prop")
-
-  info_dict["vendor.prop"] = common.LoadDictionaryFromLines(data.split("\n"))
-
-  print("--- *************** ---")
-  common.DumpInfoDict(info_dict)
-
-  return True
-
-def GetBuildProp(prop, info_dict):
-  """Return the fingerprint of the build of a given target-files info_dict."""
-  try:
-    return info_dict.get("build.prop", {})[prop]
-  except KeyError:
-    print "couldn't find %s in build.prop, try vendor.prop" %(prop)
-    #raise common.ExternalError("couldn't find %s in build.prop" % (prop,))
-
-  try:
-    return info_dict.get("vendor.prop", {})[prop]
-  except KeyError:
-    raise common.ExternalError("couldn't find %s in build.prop & vendor.prop" % (prop,))
-
 def HasTargetImage(target_files_zip, image_path):
   try:
     target_files_zip.getinfo(image_path)
     return True
   except KeyError:
     return False
-
-def BuildExt4(name, input_dir, info_dict, block_list=None):
-  """Build the (sparse) vendor image and return the name of a temp
-  file containing it."""
-  return add_img_to_target_files.CreateImage(input_dir, info_dict, name, block_list=block_list)
 
 def ZipOtherImage(which, tmpdir, output):
   """Returns an image object from IMAGES.
@@ -110,73 +78,6 @@ def GetImage(which, tmpdir):
 
   return sparse_img.SparseImage(path, mappath, clobbered_blocks)
 
-def mycopyfile(srcfile, dstfile):
-    if not os.path.isfile(srcfile):
-        print "%s not exist!" %(srcfile)
-    else:
-        fpath,fname=os.path.split(dstfile)
-        if not os.path.exists(fpath):
-            os.makedirs(fpath)
-        shutil.copyfile(srcfile,dstfile)
-        print "copy %s -> %s" %( srcfile,dstfile)
-
-
-def HasOdmPartition(target_files_zip):
-  try:
-    target_files_zip.getinfo("ODM/")
-    return True
-  except KeyError:
-    return False
-
-def BuildCustomerImage(info):
-  print "amlogic extensions:BuildCustomerImage"
-  if info.info_dict.get("update_user_parts") == "true" :
-    partsList = info.info_dict.get("user_parts_list");
-    for list_i in partsList.split(' '):
-      tmp_tgt = GetImage(list_i, info.input_tmp, info.info_dict)
-      tmp_tgt.ResetFileMap()
-      tmp_diff = common.BlockDifference(list_i, tmp_tgt, src = None)
-      tmp_diff.WriteScript(info.script,info.output_zip)
-
-def BuildCustomerIncrementalImage(info, *par, **dictarg):
-  print "amlogic extensions:BuildCustomerIncrementalImage"
-  fun = []
-  for pp in par:
-    fun.append(pp)
-  if info.info_dict.get("update_user_parts") == "true" :
-    partsList = info.info_dict.get("user_parts_list");
-    for list_i in partsList.split(' '):
-      if HasTargetImage(info.source_zip, list_i.upper() + "/"):
-        tmp_diff = fun[0](list_i, info.source_zip, info.target_zip, info.output_zip)
-        recovery_mount_options = common.OPTIONS.info_dict.get("recovery_mount_options")
-        info.script.Mount("/"+list_i, recovery_mount_options)
-        so_far = tmp_diff.EmitVerification(info.script)
-        size = []
-        if tmp_diff.patch_list:
-          size.append(tmp_diff.largest_source_size)
-        tmp_diff.RemoveUnneededFiles(info.script)
-        total_patch_size = 1.0 + tmp_diff.TotalPatchSize()
-        total_patch_size += tmp_diff.TotalPatchSize()
-        tmp_diff.EmitPatches(info.script, total_patch_size, 0)
-        tmp_items = fun[1](list_i, "META/" + list_i + "_filesystem_config.txt")
-
-        fun[2](tmp_items, info.target_zip, None)
-        temp_script = info.script.MakeTemporary()
-        tmp_items.GetMetadata(info.target_zip)
-        tmp_items.Get(list_i).SetPermissions(temp_script)
-        fun[2](tmp_items, info.source_zip, None)
-        if tmp_diff and tmp_diff.verbatim_targets:
-          info.script.Print("Unpacking new files...")
-          info.script.UnpackPackageDir(list_i, "/" + list_i)
-
-        tmp_diff.EmitRenames(info.script)
-        if common.OPTIONS.verify and tmp_diff:
-          info.script.Print("Remounting and verifying partition files...")
-          info.script.Unmount("/" + list_i)
-          info.script.Mount("/" + list_i)
-          tmp_diff.EmitExplicitTargetVerification(info.script)
-
-
 def FullOTA_Assertions(info):
   print "amlogic extensions:FullOTA_Assertions"
   try:
@@ -207,15 +108,7 @@ def FullOTA_Assertions(info):
 
 def FullOTA_InstallBegin(info):
   print "amlogic extensions:FullOTA_InstallBegin"
-  LoadInfoDict_amlogic(info.info_dict, info.input_zip);
-  platform = GetBuildProp("ro.board.platform", info.info_dict)
-  print "ro.board.platform: %s" % (platform)
-  if "meson3" in platform:
-    SetBootloaderEnv(info.script, "upgrade_step", "0")
-  elif "meson6" in platform:
-    SetBootloaderEnv(info.script, "upgrade_step", "0")
-  else:
-    SetBootloaderEnv(info.script, "upgrade_step", "3")
+  SetBootloaderEnv(info.script, "upgrade_step", "3")
 
 def FullOTA_InstallEnd(info):
   print "amlogic extensions:FullOTA_InstallEnd"
@@ -284,16 +177,8 @@ def IncrementalOTA_VerifyEnd(info):
   print "amlogic extensions:IncrementalOTA_VerifyEnd"
 
 def IncrementalOTA_InstallBegin(info):
-  LoadInfoDict_amlogic(info.info_dict, info.target_zip);
-  platform = GetBuildProp("ro.board.platform", info.info_dict)
-  print "ro.board.platform: %s" % (platform)
-  if "meson3" in platform:
-    SetBootloaderEnv(info.script, "upgrade_step", "0")
-  elif "meson6" in platform:
-    SetBootloaderEnv(info.script, "upgrade_step", "0")
-  else:
-    SetBootloaderEnv(info.script, "upgrade_step", "3")
   print "amlogic extensions:IncrementalOTA_InstallBegin"
+  SetBootloaderEnv(info.script, "upgrade_step", "3")
 
 def IncrementalOTA_ImageCheck(info, name):
   source_image = False; target_image = False; updating_image = False;
