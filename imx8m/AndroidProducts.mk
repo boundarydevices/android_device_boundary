fd4ccc7ebef891e63344f8a3e18150e5563d5566
Roberto Sartori <roberto.sartori@kynetics.com>
2019-03-29 09:41:02 +0000

    imx8m: remove the aiy_8mq and the evk_8mm folders
    Support only 8mq for now.
    
    Signed-off-by: Roberto Sartori <roberto.sartori@kynetics.com>

--- imx8m/AndroidProducts.mk
+++ imx8m/AndroidProducts.mk
@@ -28,8 +28,5 @@
 
 PRODUCT_MAKEFILES := \
   $(LOCAL_DIR)/evk_8mq/evk_8mq.mk \
-  $(LOCAL_DIR)/evk_8mq/evk_8mq_drm.mk \
-  $(LOCAL_DIR)/evk_8mm/evk_8mm.mk \
-  $(LOCAL_DIR)/evk_8mm/evk_8mm_drm.mk \
-  $(LOCAL_DIR)/aiy_8mq/aiy_8mq.mk
+  $(LOCAL_DIR)/evk_8mq/evk_8mq_drm.mk
 
--- imx8m/vendorsetup.sh
+++ imx8m/vendorsetup.sh
@@ -2,9 +2,3 @@ add_lunch_combo evk_8mq-eng
 add_lunch_combo evk_8mq-userdebug
 add_lunch_combo evk_8mq_drm-eng
 add_lunch_combo evk_8mq_drm-userdebug
-add_lunch_combo evk_8mm-eng
-add_lunch_combo evk_8mm-userdebug
-add_lunch_combo evk_8mm_drm-eng
-add_lunch_combo evk_8mm_drm-userdebug
-add_lunch_combo aiy_8mq-eng
-add_lunch_combo aiy_8mq-userdebug
