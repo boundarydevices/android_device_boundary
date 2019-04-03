84de40b6cfee4a95773679ea4acc4bd7b5dd2f70
Roberto Sartori <roberto.sartori@kynetics.com>
2019-04-03 14:26:40 +0000

    common: add the ramdisk makefile
    Allow building and including the ramdisk image in the boot partition.
    
    Signed-off-by: Roberto Sartori <roberto.sartori@kynetics.com>

---
+++ common/build/ramdisk.mk
@@ -0,0 +1,12 @@
+ifeq ($(BOARD_SOC_CLASS),IMX6)
+RAMDISK_ARCH := arm
+else
+RAMDISK_ARCH := arm64
+endif
+
+RAMDISK_TARGET := $(PRODUCT_OUT)/boot/uramdisk.img
+$(RAMDISK_TARGET): $(PRODUCT_OUT)/ramdisk.img
+	mkdir -p $(dir $@)
+	mkimage -A $(RAMDISK_ARCH) -O linux -T ramdisk -n "RAM Disk" -d $< $@
+
+bootimage: $(RAMDISK_TARGET)
