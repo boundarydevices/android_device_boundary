67a1f3579a7481bf038b7b81db78b26dcff09809
Roberto Sartori <roberto.sartori@kynetics.com>
2019-04-03 14:29:52 +0000

    nitrogen8m: do not build the dtbo image
    Signed-off-by: Roberto Sartori <roberto.sartori@kynetics.com>

--- imx8m/nitrogen8m/AndroidBoard.mk
+++ imx8m/nitrogen8m/AndroidBoard.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include device/boundary/common/build/kernel.mk
 include device/boundary/common/build/uboot.mk
-include device/boundary/common/build/dtbo.mk
 include device/boundary/common/build/imx-recovery.mk
 include device/boundary/common/build/gpt.mk
 include device/boundary/common/build/bootscript.mk
