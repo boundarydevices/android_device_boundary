1f3592e072665cfbeec24217ad42b81f712766f8
Amirhossein Sayyah <asayyah@boundarydevices.com>
2019-04-23 14:09:44 -0700

    nitrogen8m: build U-Boot and include it in the boot image
    The imx-mkimage project needs to be on the
    'boundary-imx_4.9.123_imx8mm_ga' branch from the Boundary github.
    
    Signed-off-by: Amirhossein Sayyah <asayyah@boundarydevices.com>

--- imx8m/nitrogen8m/AndroidUboot.mk
+++ imx8m/nitrogen8m/AndroidUboot.mk
@@ -6,13 +6,13 @@ ATF_CROSS_COMPILE := $(ATF_TOOLCHAIN_ABS)/aarch64-linux-androidkernel-
 define build_imx_uboot
 	$(hide) echo Building i.MX U-Boot with firmware; \
 	cd $(IMX_MKIMAGE_PATH)/imx-mkimage/; \
-	UBOOT_PATH=../../../$(UBOOT_OUT) ./make_boundary.sh; \
+	UBOOT_PATH=../../../$(UBOOT_OUT) UBOOT_DTB=imx8mq-nitrogen8mq.dtb SOC=iMX8MQ ./make_boundary.sh; \
 	cd -; \
 	SZ=$(patsubst %_,,$(strip $(2))); \
 	if [ "$$SZ" = "$(strip $(2))" ] ; then \
 		SZ=2g; \
 	fi; \
-	cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-$$SZ.hdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8m;
+	cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MQ-$$SZ.hdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8m;
 endef
 
 
