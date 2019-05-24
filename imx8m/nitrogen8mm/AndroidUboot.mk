8fb38cf5d783fc2a20838677e38a28704e169c03
Amirhossein Sayyah <asayyah@boundarydevices.com>
2019-05-24 09:31:09 -0700

    nitrogen8mm: Create uboot for som boards and higher memory
    Signed-off-by: Amirhossein Sayyah <asayyah@boundarydevices.com>

--- imx8m/nitrogen8mm/AndroidUboot.mk
+++ imx8m/nitrogen8mm/AndroidUboot.mk
@@ -6,13 +6,25 @@ ATF_CROSS_COMPILE := $(ATF_TOOLCHAIN_ABS)/aarch64-linux-androidkernel-
 define build_imx_uboot
 	$(hide) echo Building i.MX U-Boot with firmware; \
 	cd $(IMX_MKIMAGE_PATH)/imx-mkimage/; \
-	UBOOT_PATH=../../../$(UBOOT_OUT) UBOOT_DTB=imx8mm-nitrogen8mm.dtb SOC=iMX8MM ./make_boundary.sh; \
-	cd -; \
-	SZ=$(patsubst %_,,$(strip $(2))); \
-	if [ "$$SZ" = "$(strip $(2))" ] ; then \
-		SZ=2g; \
+	SZ="2g"; \
+	isSom=false; \
+	if [[ $3 =~ "som" ]]; then \
+		isSom=true; \
 	fi; \
-	cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MM-$$SZ.nohdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8mm;
+	if [[ $3 =~ "3g" ]]; then \
+		SZ="3g"; \
+	elif [[ $3 =~ "4g" ]]; then \
+		SZ="4g"; \
+	fi; \
+	if [ $$isSom = true ]; then \
+		UBOOT_PATH=../../../$(UBOOT_OUT) UBOOT_DTB=imx8mm-nitrogen8mm_som.dtb SOC=iMX8MM ./make_boundary.sh $$SZ; \
+		cd -; \
+		cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MM-$$SZ.nohdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8mm_som_$$SZ; \
+	else \
+		UBOOT_PATH=../../../$(UBOOT_OUT) UBOOT_DTB=imx8mm-nitrogen8mm.dtb SOC=iMX8MM ./make_boundary.sh $$SZ; \
+		cd -; \
+		cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MM-$$SZ.nohdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8mm_$$SZ; \
+	fi;
 endef
 
 
