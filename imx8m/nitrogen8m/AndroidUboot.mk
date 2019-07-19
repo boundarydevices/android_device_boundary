ac47de54d8669290f3d20ad7f346494243a5a0db
Amirhossein Sayyah <asayyah@boundarydevices.com>
2019-07-18 17:51:00 -0700

    nitrogen8m: rename uboot based on "UBOOT_CONFIG" passed from common/build/uboot.mk
    Signed-off-by: Amirhossein Sayyah <asayyah@boundarydevices.com>

--- imx8m/nitrogen8m/AndroidUboot.mk
+++ imx8m/nitrogen8m/AndroidUboot.mk
@@ -6,29 +6,18 @@ ATF_CROSS_COMPILE := $(ATF_TOOLCHAIN_ABS)/aarch64-linux-androidkernel-
 define build_imx_uboot
 	$(hide) echo Building i.MX U-Boot with firmware; \
 	cd $(IMX_MKIMAGE_PATH)/imx-mkimage/; \
-	SZ="2g"; \
-	isSom=false; \
+	isSom=""; \
 	if [[ $3 =~ "som" ]]; then \
-		isSom=true; \
+		isSom="_som"; \
 	fi; \
+	SZ="2g"; \
 	if [[ $3 =~ "3g" ]]; then \
 		SZ="3g"; \
 	elif [[ $3 =~ "4g" ]]; then \
 		SZ="4g"; \
 	fi; \
-	if [ $$isSom = true ]; then \
-		UBOOT_PATH=../../../$(UBOOT_OUT) UBOOT_DTB=imx8mq-nitrogen8m_som.dtb SOC=iMX8MQ ./make_boundary.sh $$SZ; \
-		cd -; \
-		cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MQ-$$SZ.hdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8m_som_$$SZ; \
-	else \
-		UBOOT_PATH=../../../$(UBOOT_OUT) UBOOT_DTB=imx8mq-nitrogen8m.dtb SOC=iMX8MQ ./make_boundary.sh $$SZ; \
-		cd -; \
-		if [ $$SZ = "2g" ]; then \
-			cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MQ-$$SZ.hdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8m; \
-		else \
-			cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MQ-$$SZ.hdmibin $(PRODUCT_OUT)/boot/u-boot.nitrogen8m_$$SZ; \
-		fi; \
-	fi;
+	UBOOT_PATH=../../../$(UBOOT_OUT) UBOOT_DTB=imx8mq-nitrogen8m$$isSom.dtb SOC=iMX8MQ ./make_boundary.sh $$SZ; \
+	cd -; \
+	uuu=`echo $3 | cut -d' ' -f2`; \
+	cp -v $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/u-boot-lpddr4-iMX8MQ-$$SZ.hdmibin $(PRODUCT_OUT)/boot/u-boot.$$uuu;
 endef
-
-
