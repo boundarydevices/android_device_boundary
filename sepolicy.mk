2c9a8dffcb3d3413255f0bb43db0c994f908af72
Gary Bisson <gary.bisson@boundarydevices.com>
2020-04-09 17:52:31 +0200

    sepolicy: remove old rules
    Will be easier to start from a clean slate.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- sepolicy.mk
+++
@@ -1,9 +0,0 @@
-ifeq ($(BOARD_SOC_CLASS),IMX6)
-BOARD_SEPOLICY_DIRS := \
-       device/boundary/sepolicy \
-       device/fsl/imx6/sepolicy
-else
-BOARD_SEPOLICY_DIRS := \
-       device/boundary/sepolicy \
-       device/fsl/imx8/sepolicy
-endif
