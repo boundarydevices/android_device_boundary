2cc457afa4ed76c31d366cc62445d2dfc89d3205
Gary Bisson <gary.bisson@boundarydevices.com>
2020-05-07 11:33:38 +0200

    nitrogen8mm: add prebuilt u-boot names for all variants
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8mm/UbootKernelBoardConfig.mk
+++ nitrogen8mm/UbootKernelBoardConfig.mk
@@ -3,6 +3,8 @@ UBOOT_POST_PROCESS := true
 
 TARGET_BOOTLOADER_CONFIG := imx8mm:nitrogen8mm_2g_defconfig
 
+TARGET_BOOTLOADER_PREBUILT := nitrogen8mm_2g nitrogen8mm_4g nitrogen8mm_rev2_2g nitrogen8mm_som_2g nitrogen8mm_som_4g
+
 TARGET_KERNEL_DEFCONFIG := boundary_android_defconfig
 
 # absolute path is used, not the same as relative path used in AOSP make
