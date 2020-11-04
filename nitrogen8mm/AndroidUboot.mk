b3cea3e059d79574e16575a0a5dff592d801e8fd
Gary Bisson <gary.bisson@boundarydevices.com>
2020-11-04 12:03:29 +0100

    nitrogen*: AndroidUboot.mk: switch ATF build to clang
    In order to fix the build, as GCC is deprecated now.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8mm/AndroidUboot.mk
+++ nitrogen8mm/AndroidUboot.mk
@@ -5,6 +5,8 @@ ATF_CROSS_COMPILE := $(strip $(AARCH64_GCC_CROSS_COMPILE))
 else
 ATF_TOOLCHAIN_ABS := $(realpath prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/bin)
 ATF_CROSS_COMPILE := $(ATF_TOOLCHAIN_ABS)/aarch64-linux-androidkernel-
+CLANG_TOOLCHAIN_ABS := $(realpath prebuilts/clang/host/linux-x86/clang-r349610/bin)
+CLANG_TO_COMPILE := CC=$(CLANG_TOOLCHAIN_ABS)/clang
 endif
 
 define build_imx_uboot
@@ -16,12 +18,12 @@ define build_imx_uboot
     fi; \
     if [ `echo $(2) | cut -d '-' -f2` = "trusty" ] && [ `echo $(2) | rev | cut -d '-' -f1` != "uuu" ]; then \
         cp $(FSL_PROPRIETARY_PATH)/fsl-proprietary/uboot-firmware/imx8m/tee-imx8mm.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/tee.bin; \
-        $(MAKE) -C $(ATF_IMX_PATH)/arm-trusted-firmware/ CROSS_COMPILE="$(ATF_CROSS_COMPILE)" PLAT=`echo $(2) | cut -d '-' -f1` bl31 -B SPD=trusty || exit 1; \
+        $(MAKE) -C $(ATF_IMX_PATH)/arm-trusted-firmware/ CROSS_COMPILE="$(ATF_CROSS_COMPILE)" $(CLANG_TO_COMPILE) PLAT=`echo $(2) | cut -d '-' -f1` bl31 -B SPD=trusty || exit 1; \
     else \
         if [ -f $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/tee.bin ] ; then \
             rm -rf $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/tee.bin; \
         fi; \
-        $(MAKE) -C $(ATF_IMX_PATH)/arm-trusted-firmware/ CROSS_COMPILE="$(ATF_CROSS_COMPILE)" PLAT=`echo $(2) | cut -d '-' -f1` bl31 -B || exit 1; \
+        $(MAKE) -C $(ATF_IMX_PATH)/arm-trusted-firmware/ CROSS_COMPILE="$(ATF_CROSS_COMPILE)" $(CLANG_TO_COMPILE) PLAT=`echo $(2) | cut -d '-' -f1` bl31 -B || exit 1; \
     fi; \
     cp $(ATF_IMX_PATH)/arm-trusted-firmware/build/`echo $(2) | cut -d '-' -f1`/release/bl31.bin $(UBOOT_OUT)/bl31-iMX8MM.bin; \
     $(MAKE) -C $(UBOOT_IMX_PATH)/uboot-imx/ CROSS_COMPILE="$(ATF_CROSS_COMPILE)" O=$(realpath $(UBOOT_OUT)) flash.bin; \
