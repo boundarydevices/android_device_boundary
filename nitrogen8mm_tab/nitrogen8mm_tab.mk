d542e8d826bb1ae6612c173e0f95149a0ac8be98
Gary Bisson <gary.bisson@boundarydevices.com>
2021-08-23 20:53:08 +0200

    nitrogen8mm_tab: enable NFC support
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8mm_tab/nitrogen8mm_tab.mk
+++ nitrogen8mm_tab/nitrogen8mm_tab.mk
@@ -288,3 +288,37 @@ PRODUCT_PACKAGES += \
     gpioinfo \
     gpiomon \
     gpioset
+
+# PN7150 config files
+NXP_NFC_HOST := $(TARGET_PRODUCT)
+NXP_NFC_HW := pn7150
+NXP_NFC_PLATFORM := pn54x
+NXP_VENDOR_DIR := nxp
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+
+# NFC config files
+PRODUCT_COPY_FILES += \
+    device/boundary/nitrogen8mm_tab/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
+    device/boundary/nitrogen8mm_tab/nfc/libnfc-nxp.conf:vendor/etc/libnfc-nxp.conf
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    libnfc-nci \
+    NfcNci \
+    Tag \
+    android.hardware.nfc@1.0-impl \
+    nfc_nci.$(NXP_NFC_PLATFORM) \
+
+# NFC HAL services
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service \
+    vendor.nxp.nxpnfc@1.0-impl \
+    vendor.nxp.nxpnfc@1.0-service
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=$(NXP_NFC_PLATFORM)
