99a64af2af8f42925818a2fe09452adad38eebad
Gary Bisson <gary.bisson@boundarydevices.com>
2021-09-30 17:00:12 +0200

    common/tools/imx-make.sh: fix vvcam module build
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- common/tools/imx-make.sh
+++ common/tools/imx-make.sh
@@ -163,7 +163,7 @@ if [ ${build_bootloader_kernel_flag} -eq 0 ] && [ ${build_android_flag} -eq 0 ];
 fi
 
 # vvcam.ko need build with kernel each time to make sure "insmod vvcam.ko" works
-if [ -n "${build_kernel}" ] && [ ${TARGET_PRODUCT} == *"8mp"* ]; then
+if [ -n "${build_kernel}" ] && [[ "${TARGET_PRODUCT}" =~ "8mp" ]]; then
     build_vvcam="vvcam";
     build_kernel_module_flag=1;
 fi
