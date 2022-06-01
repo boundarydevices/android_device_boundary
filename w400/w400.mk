df828f21f70416658d8356ddb8cbc481c058f8a9
Gary Bisson <gary.bisson@boundarydevices.com>
2022-06-01 23:10:05 +0200

    w400: remove auto patch mechanism
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- w400/w400.mk
+++ w400/w400.mk
@@ -485,27 +485,6 @@ PRODUCT_PACKAGES += \
 	libVSC \
 	libOvx12VXCBinary
 #endif
-#########################################################################
-#
-#                                     Auto Patch
-#                          must put in the end of mk files
-#########################################################################
-AUTO_PATCH_SHELL_FILE := vendor/amlogic/common/tools/auto_patch/auto_patch.sh
-AUTO_PATCH_AB := vendor/amlogic/common/tools/auto_patch/auto_patch_ab.sh
-HAVE_WRITED_SHELL_FILE := $(shell test -f $(AUTO_PATCH_SHELL_FILE) && echo yes)
-
-ifneq ($(TARGET_BUILD_LIVETV),true)
-TARGET_BUILD_LIVETV := false
-endif
-ifneq ($(TARGET_BUILD_GOOGLE_ATV),true)
-TARGET_BUILD_GOOGLE_ATV := false
-endif
-ifeq ($(HAVE_WRITED_SHELL_FILE),yes)
-$(warning $(shell ($(AUTO_PATCH_SHELL_FILE) $(TARGET_BUILD_LIVETV) $(TARGET_BUILD_GOOGLE_ATV))))
-ifeq ($(AB_OTA_UPDATER),true)
-$(warning $(shell ($(AUTO_PATCH_AB) $(PRODUCT_DIR))))
-endif
-endif
 
 PRODUCT_PACKAGES += \
     Settings \
