dba0aedb1b4fab3697ab335e4b6b52531fa9c579
Gary Bisson <gary.bisson@boundarydevices.com>
2022-11-21 11:17:59 +0100

    nitrogen8mn: remove obsolete target
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -31,7 +31,6 @@ PRODUCT_MAKEFILES += \
   $(LOCAL_DIR)/nitrogen8m/nitrogen8m.mk \
   $(LOCAL_DIR)/nitrogen8mm/nitrogen8mm.mk \
   $(LOCAL_DIR)/nitrogen8mm_tab/nitrogen8mm_tab.mk \
-  $(LOCAL_DIR)/nitrogen8mn/nitrogen8mn.mk \
   $(LOCAL_DIR)/nitrogen8mp/nitrogen8mp.mk \
   $(LOCAL_DIR)/nitrogen8ulp/nitrogen8ulp.mk \
 
@@ -44,8 +43,6 @@ COMMON_LUNCH_CHOICES := \
   nitrogen8mm-userdebug \
   nitrogen8mm_tab-user \
   nitrogen8mm_tab-userdebug \
-  nitrogen8mn-user \
-  nitrogen8mn-userdebug \
   nitrogen8mp-user \
   nitrogen8mp-userdebug \
   nitrogen8ulp-user \
