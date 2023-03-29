6013e31e23677bca5a8ad5b813ad71afe417180b
Gary Bisson <gary.bisson@boundarydevices.com>
2023-03-29 16:02:51 +0200

    nitrogen8m: fixup wifi/bt standard lib
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8m/nitrogen8m.mk
+++ nitrogen8m/nitrogen8m.mk
@@ -314,6 +314,7 @@ PRODUCT_COPY_FILES += \
 
 # -------@block_wifi-------
 PRODUCT_COPY_FILES += \
+    $(CONFIG_REPO_PATH)/common/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(CONFIG_REPO_PATH)/common/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(CONFIG_REPO_PATH)/common/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
@@ -337,8 +338,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1-service.btlinux \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
-
-BOARD_CUSTOM_BT_CONFIG := $(IMX_DEVICE_PATH)/bluetooth/libbt_vnd.conf
 
 # -------@block_usb-------
 # Usb HAL
