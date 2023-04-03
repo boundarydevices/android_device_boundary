85559f07250ef693d1423fa24c95a2708dbe2fbe
Gary Bisson <gary.bisson@boundarydevices.com>
2023-04-03 21:37:12 +0200

    nitrogen8mm: add missing bt modules
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8mm/SharedBoardConfig.mk
+++ nitrogen8mm/SharedBoardConfig.mk
@@ -12,6 +12,13 @@ LOW_MEMORY := false
 PRODUCT_IMX_TRUSTY := false
 
 # -------@block_kernel-------
+# Bluetooth driver moduls
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(KERNEL_OUT)/drivers/bluetooth/btbcm.ko \
+    $(KERNEL_OUT)/drivers/bluetooth/btqca.ko \
+    $(KERNEL_OUT)/drivers/bluetooth/hci_uart.ko
+
+# QCA9377 wifi driver module
 BOARD_HAS_QCACLD_MODULE := true
 BOARD_VENDOR_KERNEL_MODULES += \
     $(TARGET_OUT_INTERMEDIATES)/QCACLD_OBJ/wlan.ko
