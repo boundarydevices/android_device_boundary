fabb0ca56ed1371fdbd620302fd10c689b47623d
Gary Bisson <gary.bisson@boundarydevices.com>
2023-06-16 11:09:54 +0200

    nitrogen8mp: add SMARC bootloader configuration
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8mp/UbootKernelBoardConfig.mk
+++ nitrogen8mp/UbootKernelBoardConfig.mk
@@ -1,8 +1,8 @@
 TARGET_BOOTLOADER_POSTFIX := bin
 UBOOT_POST_PROCESS := true
 
-TARGET_BOOTLOADER_CONFIG := imx8mp-trusty:nitrogen8mp_2gr0_defconfig imx8mp-trusty:u-boot.nitrogen8mp_4g
-TARGET_BOOTLOADER_PREBUILT := nitrogen8mp_2gr0 nitrogen8mp_4g
+TARGET_BOOTLOADER_CONFIG := imx8mp-trusty:nitrogen8mp_2gr0_defconfig imx8mp-trusty:nitrogen8mp_4g_defconfig imx8mp-trusty:imx8mp_nitrogen_smarc_2gr0_defconfig
+TARGET_BOOTLOADER_PREBUILT := nitrogen8mp_2gr0 nitrogen8mp_4g imx8mp_nitrogen_smarc_2gr0
 
 TARGET_KERNEL_DEFCONFIG := boundary_android_defconfig
 
