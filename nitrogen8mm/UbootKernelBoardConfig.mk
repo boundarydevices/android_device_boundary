0688e30ac1f3b1280b8f2a8df44df165101bbfe9
Gary Bisson <gary.bisson@boundarydevices.com>
2023-11-29 21:12:01 +0100

    nitrogen8mm: fix prebuilt bootloader target
    This is what happens when you don't test your change ;)
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8mm/UbootKernelBoardConfig.mk
+++ nitrogen8mm/UbootKernelBoardConfig.mk
@@ -3,8 +3,7 @@ UBOOT_POST_PROCESS := true
 
 TARGET_BOOTLOADER_CONFIG := imx8mm:nitrogen8mm_2g_defconfig
 
-TARGET_BOOTLOADER_PREBUILT := nitrogen8mm_2g nitrogen8mm_4g nitrogen8mm_rev2_2g \
-				nitrogen8mm_som_2g nitrogen8mm_som_4g imx8mm_nitrogen_smarc_2gr0
+TARGET_BOOTLOADER_PREBUILT := nitrogen8mm_2g nitrogen8mm_4g nitrogen8mm_rev2_2g nitrogen8mm_som_2g nitrogen8mm_som_4g imx8mm_nitrogen_smarc_2gr0
 
 TARGET_KERNEL_DEFCONFIG := boundary_android_defconfig
 
