48b805cc3083279bf878383b6fba23f2d928bf3a
Gary Bisson <gary.bisson@boundarydevices.com>
2024-04-08 22:08:22 +0200

    nitrogen8m: switch to backports modules
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- nitrogen8m/SharedBoardConfig.mk
+++ nitrogen8m/SharedBoardConfig.mk
@@ -6,16 +6,9 @@ TARGET_KERNEL_ARCH := arm64
 #Enable this to disable product partition build.
 IMX_NO_PRODUCT_PARTITION := true
 
-# Bluetooth driver moduls
+# Wi-Fi & Bluetooth driver modules
 BOARD_VENDOR_KERNEL_MODULES += \
-    $(KERNEL_OUT)/drivers/bluetooth/btbcm.ko \
-    $(KERNEL_OUT)/drivers/bluetooth/btqca.ko \
-    $(KERNEL_OUT)/drivers/bluetooth/hci_uart.ko
-
-# QCA9377 wifi driver module
-BOARD_HAS_QCACLD_MODULE := true
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(TARGET_OUT_INTERMEDIATES)/QCACLD_OBJ/wlan.ko
+    $(wildcard $(PRODUCT_OUT)/obj/BACKPORTS_OBJ/*.ko)
 
 # Dummy battery module
 BOARD_VENDOR_KERNEL_MODULES += \
