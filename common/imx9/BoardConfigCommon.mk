0b2ff71bd15a5f4a9f9c7a05f6ef440ac74e0503
Gary Bisson <gary.bisson@ezurio.com>
2025-05-12 20:39:35 +0200

    common/imx9: initial addition
    Signed-off-by: Gary Bisson <gary.bisson@ezurio.com>

---
+++ common/imx9/BoardConfigCommon.mk
@@ -0,0 +1,241 @@
+# -------@block_common_config-------
+ENABLE_DMABUF_HEAP := true
+
+SOONG_CONFIG_NAMESPACES += IMXPLUGIN
+SOONG_CONFIG_IMXPLUGIN += BOARD_PLATFORM \
+NUM_FRAMEBUFFER_SURFACE_BUFFERS \
+BOARD_SOC_CLASS \
+HAVE_FSL_IMX_GPU3D \
+PREBUILT_FSL_IMX_GPU_MALI \
+PREBUILT_FSL_IMX_ISP \
+BOARD_SOC_TYPE \
+PRODUCT_MANUFACTURER \
+BOARD_VPU_ONLY \
+BOARD_HAVE_VPU \
+PREBUILT_FSL_IMX_CODEC \
+ENABLE_DMABUF_HEAP
+
+SOONG_CONFIG_IMXPLUGIN_BOARD_PLATFORM = imx9
+SOONG_CONFIG_IMXPLUGIN_BOARD_SOC_CLASS = IMX9
+SOONG_CONFIG_IMXPLUGIN_ENABLE_DMABUF_HEAP = true
+
+BOARD_USE_CUSTOMIZED_MESA := true
+
+#
+# Product-specific compile-time definitions.
+#
+
+ifeq ($(IMX_BUILD_32BIT_ROOTFS),true)
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_USES_64_BIT_BINDER := true
+else
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+ifneq ($(filter TRUE true 1,$(IMX_BUILD_32BIT_64BIT_ROOTFS)),)
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+endif
+endif
+
+BOARD_SOC_CLASS := IMX9
+SOONG_CONFIG_IMXPLUGIN_PRODUCT_MANUFACTURER = nxp
+
+# -------@block_kernel_bootimg-------
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := true
+TARGET_NO_RADIOIMAGE := true
+
+
+BOARD_KERNEL_OFFSET := 0x00080000
+BOARD_RAMDISK_OFFSET := 0x04280000
+ifeq ($(TARGET_USE_VENDOR_BOOT),true)
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_INIT_BOOT_HEADER_VERSION := 4
+BOARD_INCLUDE_DTB_IN_BOOTIMG := false
+else
+BOARD_BOOT_HEADER_VERSION := 1
+endif
+
+BOARD_MKBOOTIMG_ARGS = --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
+
+ifeq ($(TARGET_USE_VENDOR_BOOT),true)
+  BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+else
+  BOARD_USES_RECOVERY_AS_BOOT := true
+endif
+
+# kernel module's copy to vendor need this folder setting
+KERNEL_OUT ?= $(OUT_DIR)/target/product/$(PRODUCT_DEVICE)/obj/KERNEL_OBJ
+
+TARGET_BOARD_KERNEL_HEADERS := $(CONFIG_REPO_PATH)/common/kernel-headers
+
+TARGET_IMX_KERNEL ?= true
+ifeq ($(TARGET_IMX_KERNEL),false)
+BOARD_PREBUILT_BOOTIMAGE := vendor/nxp-opensource/imx-gki/boot_95.img
+TARGET_NO_KERNEL := true
+endif
+
+# -------@block_app-------
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
+# -------@block_storage-------
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
+ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
+AB_OTA_PARTITIONS += dtbo boot system system_dlkm system_ext vendor vendor_dlkm vbmeta
+else
+ifeq ($(TARGET_USE_VENDOR_BOOT),true)
+AB_OTA_PARTITIONS += dtbo boot init_boot vendor_boot system system_dlkm system_ext vendor vendor_dlkm vbmeta product
+else
+AB_OTA_PARTITIONS += dtbo boot system system_dlkm system_ext vendor vendor_dlkm vbmeta product
+endif
+endif
+endif
+
+ifeq ($(BOARD_HAVE_PREBOOTIMAGE),true)
+BOARD_PREBOOTIMAGE_PARTITION_SIZE := 16777216
+endif
+
+BOARD_DTBOIMG_PARTITION_SIZE := 4194304
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+ifeq ($(TARGET_USE_VENDOR_BOOT),true)
+BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE := 8388608
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
+endif
+
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE = ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+ifneq ($(IMX_NO_PRODUCT_PARTITION),true)
+  BOARD_USES_PRODUCTIMAGE := true
+  BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+  TARGET_COPY_OUT_PRODUCT := product
+endif
+
+ifeq ($(BOARD_USES_SYSTEM_EXTIMAGE),true)
+# Build a separate system_ext.img partition
+BOARD_USES_SYSTEM_EXTIMAGE := true
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+endif
+
+ifeq ($(AB_OTA_UPDATER),true)
+# Build a separate vendor_dlkm partition
+BOARD_USES_VENDOR_DLKMIMAGE := true
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+
+# Build a separate system_dlkm partition
+BOARD_USES_SYSTEM_DLKMIMAGE := false
+BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
+TARGET_COPY_OUT_SYSTEM_DLKM := system_dlkm
+TARGET_GKI_SYSTEM_DLKM ?= true
+ifeq ($(TARGET_GKI_SYSTEM_DLKM),true)
+BOARD_SYSTEM_KERNEL_MODULES += $(wildcard vendor/nxp-opensource/imx-gki/system_dlkm_staging_95/flatten/lib/modules/*.ko)
+endif
+endif
+
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
+  BOARD_SUPER_PARTITION_GROUPS := nxp_dynamic_partitions
+  BOARD_SUPER_PARTITION_SIZE := 6442450944
+  BOARD_NXP_DYNAMIC_PARTITIONS_SIZE := 6431965184
+  ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
+    ifeq ($(BOARD_USES_SYSTEM_EXTIMAGE),false)
+    BOARD_NXP_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor
+    else
+    BOARD_NXP_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_dlkm system_ext vendor vendor_dlkm
+    endif
+  else
+    BOARD_NXP_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_dlkm system_ext vendor vendor_dlkm product
+
+  endif
+  BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 0
+  BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 0
+  BOARD_SYSTEM_DLKMIMAGE_PARTITION_RESERVED_SIZE := 0
+  BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 0
+  BOARD_VENDOR_DLKMIMAGE_PARTITION_RESERVED_SIZE := 0
+  BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 0
+else
+  BOARD_VENDORIMAGE_PARTITION_SIZE := 805306368
+  BOARD_SYSTEM_EXTIMAGE_PARTITION_SIZE := 134217728
+  ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
+    BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2952790016
+  else
+    BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
+
+    BOARD_PRODUCTIMAGE_PARTITION_SIZE := 1879048192
+  endif
+endif
+
+# -------@block_bluetooth-------
+BOARD_HAVE_BLUETOOTH := true
+
+# -------@block_camera-------
+BOARD_HAVE_IMX_CAMERA := true
+
+# -------@block_display-------
+SOONG_CONFIG_IMXPLUGIN_NUM_FRAMEBUFFER_SURFACE_BUFFERS = 3
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_RECOVERY_UI_LIB := librecovery_ui_imx
+
+PREBUILT_FSL_IMX_GPU_MALI := true
+SOONG_CONFIG_IMXPLUGIN_PREBUILT_FSL_IMX_GPU_MALI = true
+
+# override some prebuilt setting if DISABLE_FSL_PREBUILT is define
+ifneq (,$(filter GPU ALL,$(DISABLE_FSL_PREBUILT)))
+    PREBUILT_FSL_IMX_GPU_MALI := false
+    SOONG_CONFIG_IMXPLUGIN_PREBUILT_FSL_IMX_GPU_MALI = false
+endif
+
+# -------@block_isp-------
+PREBUILT_FSL_IMX_ISP := true
+SOONG_CONFIG_IMXPLUGIN_PREBUILT_FSL_IMX_ISP = true
+
+# override some prebuilt setting if DISABLE_FSL_PREBUILT is define
+ifneq (,$(filter ISP ALL,$(DISABLE_FSL_PREBUILT)))
+    PREBUILT_FSL_IMX_ISP := false
+    SOONG_CONFIG_IMXPLUGIN_PREBUILT_FSL_IMX_ISP = false
+endif
+
+# -------@block_treble-------
+BOARD_VNDK_VERSION := current
+
+# -------@block_build-------
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
+# -------@block_multimedia_codec-------
+
+-include $(FSL_RESTRICTED_CODEC_PATH)/fsl-restricted-codec/fsl_ms_codec/BoardConfig.mk
+-include $(FSL_RESTRICTED_CODEC_PATH)/fsl-restricted-codec/fsl_real_dec/BoardConfig.mk
+
+ifeq ($(TARGET_USE_VENDOR_BOOT),true)
+BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
+endif
+
+# Set Vendor SPL to match platform
+VENDOR_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
+
+# Set boot SPL
+BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
